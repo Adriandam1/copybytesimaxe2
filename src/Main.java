557a57b912d24de77b7b6340e8672d0900f3a136
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2024-09-19 20:13:34 +0200

    BufferedInputStream, confirmado aumento de velocidad

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,29 @@
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         // Mensaje de inicio
         System.out.println("Iniciando el proceso de copia... crucemos los dedos");
         try (
-                // creo e indico los FileInputStream y FileOutputStream
+                // Creo los FileInputStream y FileOutputStream
                 FileInputStream inputStream = new FileInputStream("foto.jpg");
-                // Pongo false para sobreescribir el archivo si lo hubiese de texto2.txt
-                //FileOutputStream outputStream = new FileOutputStream("foto2.jpg", false);
-
-                // Utilizo true para que añada al final en vez de sobreescribir
                 FileOutputStream outputStream = new FileOutputStream("foto2.jpg", true);
-        ){
+
+                // Creo los BufferedInputStream y BufferedOutputStream
+                BufferedInputStream fileInputStream = new BufferedInputStream(inputStream);
+                BufferedOutputStream fileOutputStream = new BufferedOutputStream(outputStream);
+        ) {
             // variable para el numero de byte
             int numeroByte;
 
-            // Bucle while para leer byte a byte el archivo texto1.txt
-            // cuando llega al final el valor es -1 con el bucle leemos hasta que el valor es -1 osea el ultimo byte
-            while ((numeroByte = inputStream.read()) != -1) {
-                // Escribir cada byte en texto2.txt
-                outputStream.write(numeroByte);
+            // Bucle para leer byte a byte usando BufferedInputStream
+            while ((numeroByte = fileInputStream.read()) != -1) {
+                fileOutputStream.write(numeroByte);
             }
-            // Mensaje de confirmacion
-            System.out.println("Copia realizada con exito!");
+
+            // Mensaje de confirmación
+            System.out.println("Copia realizada con éxito!");
 
             // catch IOException
         } catch (IOException e) {
